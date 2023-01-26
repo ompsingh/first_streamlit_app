a504d3b07265b93a93e31089fc5dc223aa6a5474
Om Prakash Singh <55798799+ompsingh@users.noreply.github.com>
2023-01-26 16:34:14 -0500

    Update streamlit_app.py
    pandas to pd

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,5 +14,5 @@ streamlit.text('1 🥝')
 
 
 import pandas as pd
-my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
