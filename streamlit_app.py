8692f9676fafc5111b25fb10e93c5020875fd9df
Om Prakash Singh <55798799+ompsingh@users.noreply.github.com>
2023-01-26 16:26:44 -0500

    Update streamlit_app.py
    my favourite

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,13 @@
 
 import streamlit
 streamlit.title('My Parents New healthy Diner')
+
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 My Fruit Smoothie 🥝🍇')
+streamlit.text('1 🍌')
+streamlit.text('10g 🍇 ')
+streamlit.text('1 🥝')
